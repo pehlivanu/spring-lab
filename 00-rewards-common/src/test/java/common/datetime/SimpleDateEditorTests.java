48b46271a67079b078c27def759589904a5f6a2e
pehlivanu <pehlivanu@gmail.com>
2024-12-11 22:38:18 +0100

    Clean up repository and apply .gitignore

--- 00-rewards-common/src/test/java/common/datetime/SimpleDateEditorTests.java
+++ 00-rewards-common/src/test/java/common/datetime/SimpleDateEditorTests.java
@@ -5,26 +5,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SimpleDateEditorTests {
+class SimpleDateEditorTests {
 
 	private SimpleDateEditor editor = new SimpleDateEditor();
 
 	@Test
-	public void testGetAsText() {
+	void testGetAsText() {
 		SimpleDate date = new SimpleDate(12, 29, 1977);
 		editor.setValue(date);
 		assertEquals("December 29, 1977", editor.getAsText());
 	}
 
 	@Test
-	public void testSetAsText() {
+	void testSetAsText() {
 		editor.setAsText("December 29, 1977");
 		SimpleDate date = (SimpleDate) editor.getValue();
 		assertEquals(new SimpleDate(12, 29, 1977), date);
 	}
 
 	@Test
-	public void testSetAsTextBogus() {
+	void testSetAsTextBogus() {
 		assertThrows(IllegalArgumentException.class, () -> {
 			editor.setAsText("December 29th, 1977");
 		});
